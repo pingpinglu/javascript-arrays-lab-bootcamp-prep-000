a778754a019f2cb13c8beb5cba90c9894b15799f
Ping Ping <ping.ping.lu@hotmail.com>
2019-06-25 21:53:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ var kittens = ["Milos", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
-  
+  return name.pop("new")
+}
+
+function destructivelyPrependKitten(name) {
+  return name.unshift("new")
 }
